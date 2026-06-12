b1d0845a1b5a89cea1045333d42ed5bd6d8b795a
tausifedits <tausifraza.it@gmail.com>
2026-06-12 10:50:42 +0530

    Restructure project into Frontend and backend folders and remove env file

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 const config = require('./config/config');
 const Product = require('./models/Product');
-const { seedDatabase } = require('../database/seed');
+const { seedDatabase } = require('../Database/seed');
 
 const app = express();
 const server = http.createServer(app);
